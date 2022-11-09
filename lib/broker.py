c10abaf1d9eb25e7dc439bff28552a7eb375f052
yunanjeong <njhgle@gmail.com>
2022-11-09 17:28:23 +0900

    library

---
+++ lib/broker.py
@@ -0,0 +1,52 @@
+"""broker.py.
+
+kafka broker 관련 유틸모음
+"""
+
+import os
+
+KAFKA_HOME = '/usr/local/kafka'
+BROKER_DEFAULT = 'localhost:9092'
+
+
+def create_topic(topic, partitions, replications=1,
+                 broker=BROKER_DEFAULT):
+    """커넥터 생성. 커넥트가 Distributed 모드일 때 사용.
+
+    Args:
+        - topic (str):         토픽명
+        - partitions (int):    파티션 수
+        - replications (int):  리플리케이션팩터 수
+        - broker (str):        {IP}:{Port}, (default:localhost:9092)
+
+    Note:
+        - 토픽 생성. 이미 토픽이 존재하면 에러만 출력 후 변화없음.
+    """
+    cmd = f'{KAFKA_HOME}/bin/kafka-topics.sh' \
+        + f' --create --topic {topic}' \
+        + f' --bootstrap-server {broker}' \
+        + f' --partitions {partitions} --replication-factor {replications}'
+    os.system(cmd)
+
+
+def delete_topic(topic, broker=BROKER_DEFAULT):
+    """토픽 삭제. 사용 주의."""
+    # server.properties에 delete.topic.enable=true 필요 (default)
+    cmd = f'{KAFKA_HOME}/bin/kafka-topics.sh' \
+        + f' --bootstrap-server {broker}' \
+        + f' --topic {topic} --delete'
+    os.system(cmd)
+
+
+def show_topics(broker=BROKER_DEFAULT):
+    cmd = f'kafkacat -b {broker} -L | grep topic'
+    os.system(cmd)
+
+
+def show_records(topic, broker=BROKER_DEFAULT):
+    cmd = f'kafkacat -b {broker} -t {topic} -q -e'
+    os.system(cmd)
+
+
+def test_fucntion():
+    print('test')
---
+++ lib/connect.py
@@ -0,0 +1,75 @@
+"""connect.py.
+
+kafka connect 관련 유틸모음
+"""
+
+import os
+import json
+import yaml
+import requests
+from time import sleep
+from datetime import datetime
+
+CONNECT_DEFAULT = 'localhost:8083'
+
+
+class Connector:
+    def __init__(self, info):
+        self.info = info
+
+    def create(self, suffix=False, connect=CONNECT_DEFAULT):
+        """커넥터 생성. 커넥트가 Distributed 모드일 때 사용.
+
+        Args:
+            - suffix (bool):    커넥터 이름에 날짜표기 유무 (default: False)
+            - connect (str):    {IP}:{Port}
+        """
+        if suffix is True:
+            now = datetime.today().strftime("%y%m%d")
+            self.set_name(self.info['name'] + '_' + now)
+        sleep(1)
+        res = send_http(f'http://{connect}/connectors', self.info)
+        print(res)
+
+    def delete(self, connect=CONNECT_DEFAULT):
+        name = str(self.info['name'])
+        cmd = f'curl {connect}/connectors/{name} -XDELETE'
+        os.system(cmd)
+
+    def show(self, connect=CONNECT_DEFAULT):
+        name = str(self.info['name'])
+        cmd = f'curl {connect}/connectors/{name} | jq | sort'
+        os.system(cmd)
+
+    def set_name(self, name):
+        self.info['name'] = name
+
+    def get_name(self):
+        return self.info['name']
+
+    def get_config(self):
+        return self.info['config']
+
+    def get_topic_prefix(self):
+        return self.info['config']['topic.prefix']
+
+
+def send_http(url, body):
+    headers = {
+        'Content-Type': 'application/json',
+        'Accept': 'application/json'
+    }
+    res = requests.post(url, json.dumps(body, ensure_ascii=False), headers=headers)  # NOQA
+    return res
+
+
+def get_yaml(filepath):
+    with open(filepath, 'r') as file:
+        data = yaml.load(file, Loader=yaml.FullLoader)
+    return data
+
+
+def get_json(filepath):
+    with open(filepath, 'r') as file:
+        data = json.load(file)
+    return data
