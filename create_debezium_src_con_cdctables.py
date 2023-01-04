e745a6450d279831a8bf658e8618a52ff1837166
yunanjeong <njhgle@gmail.com>
2023-01-04 11:42:30 +0900

    cdc test

---
+++ create_debezium_src_con_cdctables.py
@@ -0,0 +1,26 @@
+"""코드테이블 커넥터 일괄 생성."""
+import os
+import lib.broker as broker
+from lib.connect import Connector, get_yaml, get_json
+
+
+WORK_DIR = os.getcwd()
+file = get_yaml(f'{WORK_DIR}/config/debezium_src/debezium_src_cdctables.yml')
+jdbc_info = get_json(f'{WORK_DIR}/config/debezium_src/debezium_info.json')
+infos = file['connectors']
+common = file['common']
+
+# jdbc 연결 정보를 공통사항에 반영
+common['config'].update(jdbc_info)
+
+# 공통사항을 모든 커넥터 정보에 반영
+for info in infos:
+    info['config'].update(common['config'])
+
+# 커넥터 생성
+for info in infos:
+    con = Connector(info)
+    broker.create_topic(topic=con.get_config()['topic.prefix'], partitions=con.get_config()['tasks.max'])  # NOQA
+    con.create()
+    # print(con.get_name())
+    # print(con.get_config())
